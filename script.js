3c549c561669aef92d1a9016f77fb6f3f67140bb
Timothy Barmann <tbarmann92@gmail.com>
2022-03-13 21:41:44 -0400

    add new mouseover functions

--- script.js
+++ script.js
@@ -14,6 +14,37 @@ var projection = d3.geoTransverseMercator()
   .scale(70000)
   .translate([500, 1150])
   .rotate([71 + 30 / 60, -41 - 5 / 60]);
+ let mouseOver = function(d) {
+    d3.select(this)
+      .raise()
+      .transition()
+      // .duration(200)
+      .style("opacity", .5)
+      .style("stroke", "black")
+      .style("stroke-width", "3");
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", .9);
+    tooltip.html(d.properties.NAME);
+  }
+
+  let mouseMove = function(d) {
+    tooltip
+      .style("left", (d3.mouse(this)[0]) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px");
+  }
+
+  let mouseLeave = function(d) {
+    d3.select(this)
+      .transition()
+      .duration(200)
+      .style("opacity", 1)
+      .style("stroke", "white")
+      .style("stroke-width", "1");
+    tooltip.transition()
+      .duration(500)
+      .style("opacity", 0);
+  }
 
 var geoGenerator = d3.geoPath()
   .projection(projection);
@@ -41,19 +72,9 @@ let drawMap = () => {
       return "gray";
     })
     .style('stroke', 'white')
-    .on("mouseover", function (d) {
-      tooltip.transition()
-        .duration(200)
-        .style("opacity", .9);
-      tooltip.html(d.properties.NAME)
-        .style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 28) + "px");
-    })
-    .on("mouseout", function (d) {
-      tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-    });
+    .on("mouseover", mouseOver)
+    .on("mouseleave", mouseLeave)
+    .on("mousemove", mouseMove);
 }
 
 d3.json(riURL).then((data, error) => {
